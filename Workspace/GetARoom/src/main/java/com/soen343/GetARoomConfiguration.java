d42bf993dbd98ee9bd6bdad7a4a5e6e7501e6ae1
Vynny <sylvain.czyzewski@gmail.com>
2016-11-07 15:13:02 -0500

    added code base

---
+++ Workspace/GetARoom/src/main/java/com/soen343/GetARoomConfiguration.java
@@ -0,0 +1,16 @@
+package com.soen343;
+
+import io.dropwizard.Configuration;
+
+public class GetARoomConfiguration extends Configuration/* implements
+        AssetsBundleConfiguration */{
+/*    @NotNull
+    @JsonProperty
+    private final AssetsConfiguration assets = new AssetsConfiguration();
+
+    @Override
+    public AssetsConfiguration getAssetsConfiguration() {
+        return assets;
+    }*/
+}
+
